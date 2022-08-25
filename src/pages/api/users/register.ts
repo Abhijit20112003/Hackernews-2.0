a39e5cba711b27b4404a0e0b78bfa67efe50cf6d
Anurag <anuragsharma011011@gmail.com>
2022-08-26 02:01:11 +0530

    register function

---
+++ src/pages/api/users/register.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../db/client";
+const bcrypt = require("bcryptjs");
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, password, password2 } = req.body;
+
+  const exists = await prisma.user.findFirst({
+    where: {
+      email: {
+        equals: email,
+      },
+    },
+  });
+
+  if (exists) {
+    res.status(400).json({ msg: "User already exists" });
+  }
+  else if(password !== password2) {
+    res.status(400).json({ msg: "Passwords do not match" });
+  }
+  else {
+    let salt:string = await bcrypt.genSalt(10);
+    let hashed_password :string = await bcrypt.hash(password, salt);
+    const user = await prisma.user.create({
+        data: {
+          email,
+          password:hashed_password,
+        },
+      });
+      res.status(201).json(user);
+  }
+};
